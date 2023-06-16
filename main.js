9a8240d3f53a2f0168aa061a33c990b8daa31515
s4rmier <135184315+s4rmier@users.noreply.github.com>
2023-06-16 11:40:51 -0700

    JS ok / Responsive ok

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 const hamMenu = document.getElementById("ham-menu");
 const modalClose = document.getElementById("modal-close");
-
 const navModal = document.querySelector(".mobile-nav-modal");
 
-modalClose.addEventListener("click", () => {
+const toggleNavModal = () => {
   navModal.classList.toggle("hide");
-});
+};
 
-hamMenu.addEventListener("click", () => {
-  navModal.classList.toggle("hide");
-});
+modalClose.addEventListener("click", toggleNavModal);
+hamMenu.addEventListener("click", toggleNavModal);
